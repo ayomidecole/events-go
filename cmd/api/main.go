cd7c2546087375f2081570719528b83eb976b657
Ayomide Aremu-Cole <121998651+ayomidecole@users.noreply.github.com>
2026-03-24 05:58:15 -0400

    Update dependencies in go.mod and go.sum; refactor main application structure to include environment configuration and database models.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,15 +2,34 @@ package main
 
 import (
 	"database/sql"
+	"events-go/internal/database"
+	"events-go/internal/env"
 	"log"
 )
 
+type application struct {
+	port int
+	jwtSecret string
+	models database.Models
+}
+
 func main() {
-	db, err != sql.sql.Open("sqlite3", "./data.db")
+	db, err := sql.Open("sqlite3", "./data.db")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	defer db.Close()
+
+	models := database.NewModels(db)
+	app := &application{
+		port: env.GetEnvInt("PORT", 8080),
+		jwtSecret: env.GetEnvString("JWT_SECRET", "some-secret-123456"),
+		models: models,
+	}
+
+	if err := app.serve(); err !=nil {
+		log.Fatal(err)
+	}
 }
